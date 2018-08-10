b2b5d8ca064fa8cfe6e45fd4d57cd0bc5f99c6ff
qiang.wen <qiang.wen@163.com>
2018-08-10 15:19:09 +0800

    添加数据操作

--- src/main/java/com/yunlian/loganalysis/dao/StatCallApiDao.java
+++ src/main/java/com/yunlian/loganalysis/dao/StatCallApiDao.java
@@ -1,8 +1,22 @@
 package com.yunlian.loganalysis.dao;
 
+import com.yunlian.loganalysis.po.StatCallApiPo;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author qiang.wen
  * @date 2018/8/9 14:56
  */
 public interface StatCallApiDao {
+
+    //批量插入
+    void insertBatch(List<StatCallApiPo> list);
+
+    //根据apiUrl更新记录
+    int updateByApiUrl(StatCallApiPo apiPo);
+
+    //根据查询参数查询排除时间的记录
+    List<StatCallApiPo> queryExcludeTime(Map<String,Object> queryMap);
 }
--- src/main/java/com/yunlian/loganalysis/service/LogDataAnalysisService.java
+++ src/main/java/com/yunlian/loganalysis/service/LogDataAnalysisService.java
@@ -2,15 +2,22 @@ package com.yunlian.loganalysis.service;
 
 import com.yunlian.loganalysis.config.DbConfig;
 import com.yunlian.loganalysis.convertor.LogDataConvertor;
+import com.yunlian.loganalysis.dao.StatCallApiDao;
+import com.yunlian.loganalysis.dao.StatCallDailyDao;
 import com.yunlian.loganalysis.dao.StatOriginLogDataDao;
 import com.yunlian.loganalysis.dto.StatOriginLogDataDto;
 import com.yunlian.loganalysis.po.*;
+import com.yunlian.loganalysis.util.GlobalIdGenerator;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.storm.shade.org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List; /**
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
  * @author qiang.wen
  * @date 2018/8/9 14:53
  *
@@ -80,12 +87,24 @@ public class LogDataAnalysisService {
         
     }
 
+    /**
+     * 处理每天的api的调用数据
+     * @param sqlSession
+     * @param statCallDailyPos
+     */
     private void handleStatCallDailyData(SqlSession sqlSession, List<StatCallDailyPo> statCallDailyPos) {
+        if(CollectionUtils.isEmpty(statCallDailyPos)){
+            return;
+        }
+        StatCallDailyDao statCallDailyDao = sqlSession.getMapper(StatCallDailyDao.class);
+        //根据statDate分组
+        Map<LocalDate,List<StatCallDailyPo>> dataMap = statCallDailyPos.stream().collect(Collectors.groupingBy(po -> po.getStatDate()));
+
         
     }
 
     /**
-     * 处理每个Api的调用情况
+     * 处理每个Api的调用数据
      * @param sqlSession
      * @param statCallApiPos
      */
@@ -93,7 +112,56 @@ public class LogDataAnalysisService {
         if(CollectionUtils.isEmpty(statCallApiPos)){
             return;
         }
-
+        StatCallApiDao statCallApiDao = sqlSession.getMapper(StatCallApiDao.class);
+        //根据apiUrl分组
+        Map<String,List<StatCallApiPo>> dataMap = statCallApiPos.stream().collect(Collectors.groupingBy(po -> po.getApiUrl()));
+        //根据apiUrl查询列表
+        Set<String> apiUrlSet = dataMap.keySet();
+        Map<String,Object> queryMap = new HashMap<>();
+        queryMap.put("apiUrlList",apiUrlSet);
+        List<StatCallApiPo> dbPos = statCallApiDao.queryExcludeTime(queryMap);
+        Map<String,StatCallApiPo> dbPoMap = new HashMap<>();
+        if(CollectionUtils.isNotEmpty(dbPos)){
+            dbPoMap = dbPos.stream().collect(Collectors.toMap(po -> po.getApiUrl(),po -> po, (newPo,oldPo) -> newPo ));
+        }
+        //分组进行处理
+        //待插入列表
+        List<StatCallApiPo> needInsertList = new ArrayList<>();
+        //待更新列表
+        List<StatCallApiPo> needUpdateList = new ArrayList<>();
+        GlobalIdGenerator idGenerator = GlobalIdGenerator.getInstance();
+        for(String apiUrl : apiUrlSet){
+            StatCallApiPo po = dbPoMap.get(apiUrl);
+            List<StatCallApiPo> poList = dataMap.get(apiUrl);
+            //统计次数
+            long totalNum = 0,successNum = 0, failureNum = 0;
+            for (StatCallApiPo apiPo : poList){
+                totalNum += apiPo.getTotalCallnum();
+                successNum += apiPo.getSuccessCallnum();
+                failureNum += apiPo.getFailureCallnum();
+            }
+            if(Objects.isNull(po)){
+                //db中不存在，需要待插入
+                StatCallApiPo tempPo = poList.get(0);
+                tempPo.setUid(String.valueOf(idGenerator.nextId()));
+                tempPo.setTotalCallnum(totalNum);
+                tempPo.setSuccessCallnum(successNum);
+                tempPo.setFailureCallnum(failureNum);
+                needInsertList.add(tempPo);
+            }else{
+                //db中存在，需要更新
+                po.setTotalCallnum(po.getTotalCallnum() + totalNum);
+                po.setFailureCallnum(po.getFailureCallnum() + failureNum);
+                po.setSuccessCallnum(po.getSuccessCallnum() + successNum);
+                needUpdateList.add(po);
+            }
+        }
+        //新增
+        statCallApiDao.insertBatch(needInsertList);
+        //更新
+        needUpdateList.forEach(updatePo -> {
+            statCallApiDao.updateByApiUrl(updatePo);
+        });
     }
 
     /**
